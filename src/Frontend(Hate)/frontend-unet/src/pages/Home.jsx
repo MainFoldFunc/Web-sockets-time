566b4f5fd9c5e3bc6ca619224bf7844617560c2b
MainFoldFunc <gs.live.pub@gmail.com>
2025-02-16 15:49:14 +0100

    Login system chiecked

--- src/Frontend(Hate)/frontend-unet/src/pages/Home.jsx
+++ src/Frontend(Hate)/frontend-unet/src/pages/Home.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
+
 function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1>Main page</h1>
+      {isLoggedIn ? (
+        <>
+          <div>
+            <input type="text" />
+            <button />
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>Welcome to the Main Page</h1>
+          <p>Please log in to access more features.</p>
+          <button onClick={() => navigate("/login")}>Login</button>
+        </>
+      )}
     </div>
   );
 }
